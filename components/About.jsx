7479cc3507b1037e73125114c29707d09d8c1a23
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-01-18 13:01:46 -0800

    Added some changes for sentene

--- components/About.jsx
+++ components/About.jsx
@@ -14,24 +14,17 @@ const About = () => {
             About
           </p>
           <h2 className="py-4">Who I Am</h2>
-          <p className="py-2 text-gray-600 text-2xl">
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+          <p className="py-2 text-gray-600 text-xl">
+            I started learning web development in 2021 with my school course
+            that are mostly HTML, CSS, JavaScript, React, Next.js, Tailwind.
+            Though I am most proficient in building front-end applications using
+            HTML, CSS, Javascript, and React, I am a quick learner and can pick
+            up new tech stacks as needed. I believe that being a great developer
+            is not using one specific language, but choosing the best tool for
+            the job. In my free time, I like to do outdoor activities such as
+            running, swimming, and playing tennis. Each activities{" "}
           </p>
-          <p className="py-2 text-gray-600 text-2xl">
-            I started web developement in 2013 managing multiple e-commerce
-            websites on CMS platforms such as WordPress, BigCommerce, and
-            Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. In my spare
-            time I run Code Commerce, a Youtube channel where I teach web
-            developement and various front-end technologies.
-          </p>
+          <p className="py-2 text-gray-600 text-xl"></p>
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer text-xl">
               Check out some of my latest projects.
