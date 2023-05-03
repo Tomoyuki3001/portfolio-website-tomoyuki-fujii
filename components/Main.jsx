d5c74ce12d05496ffb5a260e2996ca87529479cd
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-05-03 13:23:31 -0700

    Added new contents

--- components/Main.jsx
+++ components/Main.jsx
@@ -30,7 +30,7 @@ const Main = () => {
           <h1 className="py-2 text-gray-700">Full-stack Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto text-xl">
             I’m a junior full-stack developer based in Vancouver. I have over
-            one year’s of experience in full-stack development.
+            2 years of experience in full-stack development.
           </p>
           <div className="flex items-center justify-between max-w-[320px] m-auto py-4">
             <a
