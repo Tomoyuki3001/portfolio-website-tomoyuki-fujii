0adc80a086d999137c7303dcd20e8f3a5361ae18
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-05-02 09:48:54 -0700

    Removed the code and updated the desing

--- components/ProjectComponent.jsx
+++ components/ProjectComponent.jsx
@@ -8,24 +8,26 @@ const ExperienceComponent = ({ project }) => {
       </div>
       <h2 className="text-xl my-3">{project.name}</h2>
       <p className="text-xl">
+        View{" "}
         <a
           href={project.demo}
           target="_blank"
           rel="noreferrer"
           className="text-xl text-[#339AF0]"
         >
-          View Demo
+          Demo{" "}
+        </a>
+        <span>{project.code === "" ? "" : "/"}</span>
+        <a
+          href={project.code}
+          target="_blank"
+          rel="noreferrer"
+          className="text-xl text-[#339AF0]"
+        >
+          {project.code === "" ? "" : " Code"}
         </a>
       </p>
       <p className="text-sm md:text-lg">{project.description}</p>
-      <a
-        href={project.code}
-        target="_blank"
-        rel="noreferrer"
-        className="text-xl text-[#339AF0]"
-      >
-        {project.code === "" ? "" : "View Code"}
-      </a>
       <p className="font-bold text-sm md:text-md mt-1">{project.skills}</p>
     </div>
   );
